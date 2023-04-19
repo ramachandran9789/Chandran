7dff2bd0a5b0fee65ea42ed5b3d388e47f85c4a2
ramachandran9789 <ramki.ramachandran@gmail.com>
2023-04-19 12:27:13 +0530

    hi

---
+++ src/com/git/Commad.java
@@ -0,0 +1,13 @@
+package com.git;
+
+public class Commad {
+	private void get() {
+		int i = 100;
+		System.out.println(i);
+	}
+	public static void main(String[] args) {
+		Commad g = new Commad();
+		g.get();
+		System.out.println("Complete");
+	}
+}
